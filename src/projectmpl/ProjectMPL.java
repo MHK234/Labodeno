46d6ad1272acba760ef62b5372f7f7d306dee2d9
MHK234 <118443020+MHK234@users.noreply.github.com>
2023-01-10 22:47:28 +0500

    Labodeno project

---
+++ src/projectmpl/ProjectMPL.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectmpl;
+
+/**
+ *
+ * @author user
+ */
+public class ProjectMPL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginScreen().setVisible(true);
+            }
+        });
+    }
+    
+}
